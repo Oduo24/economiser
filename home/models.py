4ae1b3dcdb1f1c9b4b4c14bc2f67d3e63c322674
Gerald <geraldoduo@email.com>
2022-01-02 11:06:14 +0300

    maximum digits for readings

--- home/models.py
+++ home/models.py
@@ -39,28 +39,28 @@ class Photocopier(models.Model):
 class MeterReading(models.Model):
     date = models.DateField()
     meter_location = models.CharField(max_length=100)
-    reading = models.DecimalField(max_digits=5, decimal_places=2)
+    reading = models.DecimalField(max_digits=9, decimal_places=2)
 
 
 class Meter1(models.Model):
     date = models.DateField()
     meter_location = models.CharField(max_length=100)
-    reading = models.DecimalField(max_digits=5, decimal_places=2)
-    consumption = models.DecimalField(max_digits=5, decimal_places=2)
+    reading = models.DecimalField(max_digits=9, decimal_places=2)
+    consumption = models.DecimalField(max_digits=9, decimal_places=2)
 
 
 class Meter2(models.Model):
     date = models.DateField()
     meter_location = models.CharField(max_length=100)
-    reading = models.DecimalField(max_digits=5, decimal_places=2)
-    consumption = models.DecimalField(max_digits=5, decimal_places=2)
+    reading = models.DecimalField(max_digits=9, decimal_places=2)
+    consumption = models.DecimalField(max_digits=9, decimal_places=2)
 
 
 class Meter3(models.Model):
     date = models.DateField()
     meter_location = models.CharField(max_length=100)
-    reading = models.DecimalField(max_digits=5, decimal_places=2)
-    consumption = models.DecimalField(max_digits=5, decimal_places=2)
+    reading = models.DecimalField(max_digits=9, decimal_places=2)
+    consumption = models.DecimalField(max_digits=9, decimal_places=2)
 
 
 
